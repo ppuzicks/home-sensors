42721bf6e74402a3b28b86a66f7fdb9126e424b4
Taehee Kim <thkim7816@gmail.com>
2022-05-20 22:01:08 +0900

    추가: st7735 lcd display 테스트코드

---
+++ home_mesh_app/src/display_test.h
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef __DISPLAY_H__
+#define __DISPLAY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+void display_test(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __DISPLAY_H__
